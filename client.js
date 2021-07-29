3b7c0d629db3ffcfb4313086c33664bb2c5a2718
Jonathan Choi <jonchoi11@gmail.com>
2021-07-29 00:01:16 +0000

    created client file

---
+++ client.js
@@ -0,0 +1,19 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133', // IP address here,
+    port: 50542// PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log('you ded cuz you idled');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
